ec5401cdfccd7d05caacc1534ee9ec4e6b82074e
Pawan Lamba <pawan88.lamba@gmail.com>
2020-06-03 15:17:47 -0400

    Product index and new page views and urls configuration

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Product Index Page')
+
+
+def new(request):
+    return HttpResponse('New Product')
+
+
+
+
+
